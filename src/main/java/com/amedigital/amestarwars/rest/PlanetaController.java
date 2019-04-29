cfc6a1c493826650ff186dd0d3c3d4abda20ecaf
Falcão <gfalcao@outlook.com>
2019-04-28 22:01:01 -0300

    Criada camada de regra de negócio, além de exceções descritivas

--- src/main/java/com/amedigital/amestarwars/rest/PlanetaController.java
+++ src/main/java/com/amedigital/amestarwars/rest/PlanetaController.java
@@ -7,17 +7,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.amedigital.amestarwars.model.Planeta;
-import com.amedigital.amestarwars.repository.PlanetaRepository;
-import com.amedigital.amestarwars.swapi.model.Planet;
+import com.amedigital.amestarwars.service.PlanetaService;
+import com.amedigital.amestarwars.service.exception.CamposNulosException;
+import com.amedigital.amestarwars.service.exception.NomeJaExistenteException;
+import com.amedigital.amestarwars.service.exception.PlanetaNaoEncontradoException;
+import com.amedigital.amestarwars.service.exception.ValorNaoNumericoException;
 
 @RestController
 @RequestMapping(value = "/api/planeta")
@@ -25,69 +28,76 @@ import com.amedigital.amestarwars.swapi.model.Planet;
 public class PlanetaController {
 
 	@Autowired
-	private PlanetaRepository planetaRepository;
+	private PlanetaService planetaService;
 
-	@GetMapping(value = "/carregaMassa")
+	@PostMapping(value = "/adicionar")
 	@CrossOrigin("*")
-	public HttpStatus carregaMassa() throws Exception {
-
-		for (int idPlaneta = 1; idPlaneta < 62; idPlaneta++) {
-
-			RestTemplate restTemplate = new RestTemplate();
-			Planet planet = restTemplate.getForObject("https://swapi.co/api/planets/" + idPlaneta, Planet.class);
-
-			planetaRepository.add(planet.planetToPlaneta());
-		}
-
-		return HttpStatus.OK;
+	public ResponseEntity<Planeta> adicionar(@RequestBody Planeta planeta) throws Exception {
+		planeta = planetaService.add(planeta);
+		return new ResponseEntity<Planeta>(planeta, HttpStatus.CREATED);
 	}
-
+	
 	@GetMapping(value = "/listar")
 	@CrossOrigin("*")
 	public ResponseEntity<List<Planeta>> listar() throws Exception {
-		List<Planeta> planetas = planetaRepository.findAll();
+		List<Planeta> planetas = planetaService.findAll();
 		return new ResponseEntity<List<Planeta>>(planetas, HttpStatus.OK);
 	}
 
 	@GetMapping(value = "/listar/nome/{nome}")
 	@CrossOrigin("*")
 	public ResponseEntity<Planeta> buscaPorNome(@PathVariable String nome) throws Exception {
-		Planeta planeta = planetaRepository.findByName(nome);
-		if (planeta == null) {
-			return new ResponseEntity<Planeta>(planeta, HttpStatus.NO_CONTENT);
-		}
-
+		Planeta planeta = planetaService.findByName(nome);
 		return new ResponseEntity<Planeta>(planeta, HttpStatus.FOUND);
 	}
 
 	@GetMapping(value = "/listar/id/{id}")
 	@CrossOrigin("*")
 	public ResponseEntity<Planeta> buscarPorId(@PathVariable String id) throws Exception {
-
-		Planeta planeta = planetaRepository.findById(Long.parseLong(id));
-
-		if (planeta == null) {
-			return new ResponseEntity<Planeta>(planeta, HttpStatus.NO_CONTENT);
-		}
-
+		Planeta planeta = planetaService.findById(id);
 		return new ResponseEntity<Planeta>(planeta, HttpStatus.FOUND);
 	}
 
-	@PostMapping(value = "/adicionar")
-	@CrossOrigin("*")
-	public ResponseEntity<Planeta> adicionar(@RequestBody Planeta planeta) throws Exception {
-		planetaRepository.add(planeta);
-		return new ResponseEntity<Planeta>(planeta, HttpStatus.CREATED);
-	}
-	
 	@DeleteMapping(value = "/deletar/{id}")
 	@CrossOrigin("*")
 	public HttpStatus remover(@PathVariable String id) throws NumberFormatException, Exception {
-		Planeta planeta = planetaRepository.findById(Long.parseLong(id));
-		if (planeta == null)
-			return HttpStatus.NO_CONTENT;
-		
-		planetaRepository.remove(planeta);
+		planetaService.remover(id);
 		return HttpStatus.ACCEPTED;
 	}
+	
+	@ExceptionHandler({NomeJaExistenteException.class, CamposNulosException.class})
+	public ResponseEntity<Erro> handleBadRequestException(Exception e) {
+		return new ResponseEntity<>(new Erro(e.getMessage()), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler({PlanetaNaoEncontradoException.class})
+	public ResponseEntity<Erro> handlePlanetaNaoEncontradoException(PlanetaNaoEncontradoException e) {
+		return new ResponseEntity<>(new Erro(e.getMessage()), HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler({ValorNaoNumericoException.class})
+	public ResponseEntity<Erro> handleValorNaoNumericoException(ValorNaoNumericoException e) {
+		return new ResponseEntity<>(new Erro(e.getMessage()), HttpStatus.BAD_REQUEST);
+	}
+	
+	@GetMapping(value = "/carregaMassa")
+	@CrossOrigin("*")
+	public HttpStatus carregaMassa() throws Exception {
+		planetaService.carregarMassa();
+		return HttpStatus.OK;
+	}
+}
+
+class Erro {
+	
+	private final String erro;
+
+	public Erro(String erro) {
+		this.erro = erro;
+	}
+
+	public String getErro() {
+		return erro;
+	}
+	
 }
